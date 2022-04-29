41e3629d35bcb1478080f6c9a70da6b3e60799d8
Junha Shin <junha.shin@wisc.edu>
2022-04-29 11:44:55 -0500

    Update run_findTransitionGenesets.sh

--- scFindTransitioning/run_findTransitionGenesets.sh
+++ scFindTransitioning/run_findTransitionGenesets.sh
@@ -1,23 +1,22 @@
-# usage	: ./run_findTransitionGenesets.sh [hclust_threshold]
-# e.g.	: ./run_findTransitionGenesets.sh 0.05
+# usage	: ./run_findTransitionGenesets.sh [input_dir_name] [matrix.txt] [order.txt] [OGID.txt]
+# e.g.	: ./run_findTransitionGenesets.sh input/ example_matrix.txt example_order.txt example_OGID.txt
+set -u
 
 THR=0.05	# Default value is 0.05. Higher threshold value allows the result gene sets having complexed patterns.
 MISSING=0	# missing is not allowed in the scFindTransitioning
 SIZE=5		# minimum size of cluster (# of genes in a gene set)
 SRCCLUST=c1	# Default name of the representative cell cluster
 
-INDIR=input/
-MATRIX=example_matrix.txt
-ORDER=example_order.txt
-OGID=example_OGID.txt
+INDIR=$1	# input dir name
+MATRIX=$2	# input matrix file, which is the result table of Arboretum
+ORDER=$3	# input order file, synced to the OGID file columns
+OGID=$4		# input OGID info file
 
-OUTDIR=thr${THR}_miss${MISSING}
+OUTDIR=thr${THR}_miss${MISSING}	# output dir will be named programatically.
 mkdir ${OUTDIR}
 echo "${OUTDIR}"
 
-cp -r input/ in/
 ./scFindTransitioning ${INDIR} ${MATRIX} ${ORDER} ${OGID} ${SRCCLUST} ${OUTDIR} ${THR} ${SIZE} ${MISSING}
-
 # Meaning of parameters
 # ./scFindTransitioning [input_dir] [matrix.txt] [order.txt] [OG.txt] [col_name] [output_dir_name] [threshold] [min_set_size] [max_missing_allow]
 #  - [input_dir]: input directory that contains matrix, order and OG information
